9036a33ef9e69534fdf317d03e8b740354003b6c
Abigail Watson <awatson1978@gmail.com>
2019-06-09 00:16:26 -0500

    initial commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hello from './Hello.jsx';
+import Info from './Info.jsx';
+import HelloNpm from 'material-fhir-ui';
+
+const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    <Hello />
+    <HelloNpm />
+  </div>
+);
+
+export default App;
