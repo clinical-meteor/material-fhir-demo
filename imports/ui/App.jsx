aa9a83466910cc28aad8cb57206d722789a69987
Abigail Watson <awatson1978@gmail.com>
2019-08-11 23:59:17 -0500

    fhir-kit-client@v1.4.0

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { PatientCard, PatientTable, PatientDetail } from 'material-fhir-ui';
+
+import React, { useState } from 'react';
+import { PatientCard, PatientTable, PatientDetail, ObservationTable } from 'material-fhir-ui';
 
 import { RaisedButton } from 'material-ui';
-
 
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {blue400, blue600} from 'material-ui/styles/colors';
+
+import Client from 'fhir-kit-client';
 
 // import get from 'lodash-es';
 import _ from 'lodash';
@@ -15,8 +17,8 @@ let set = _.set;
 
 const muiTheme = getMuiTheme({
   palette: {
-    primary1Color: blue400,
-    primary2Color: blue600,
+    primary2Color: blue400,
+    primary1Color: blue600,
     pickerHeaderColor: blue600,
     background: "#dddddd"
   }
@@ -61,52 +63,106 @@ let janeDoe = {
 
 let patients = [janeDoe]
 
+let a1c = {
+  "resourceType": "Observation",
+  "_id": "RcQY5EtPxCsLBLqsr",
+  "status": "final",
+  "category": {
+    "text": "Vital Signs"
+  },
+  "effectiveDateTime": "2019-06-23T01:57:10.688Z",
+  "subject": {
+    "display": "Jane Doe",
+    "reference": "Patient/12345"
+  },
+  "performer": [],
+  "device": {
+    "display": "",
+    "reference": ""
+  },
+  "valueQuantity": {
+    "value": "212",
+    "unit": "mg/dL",
+    "system": "http://unitsofmeasure.org"
+  },
+  "valueString": "",
+  "code": {
+    "text": "HbA1c",
+    "coding": [
+      {
+        "code": "4548-4"
+      }
+    ]
+  }
+};
+
+let observations = [a1c]
 
 
+onButtonClick = function(fhirClient, setCapabilityStatement){
+  console.log('onButtonClick')
+  console.log('fhirClient', fhirClient)
+  console.log('fhirClient.smartAuthMetadata()', fhirClient.smartAuthMetadata())
+  fhirClient.smartAuthMetadata().then((response) => {
+    console.log('smartAuthMetadata', response);
+  });
+  fhirClient.capabilityStatement().then((data) => {
+    setCapabilityStatement(JSON.stringify(data, null, ' '));
+  });  
+}
 
-const App = () => (
-  <div style={{width: '100%', height: '100%', top: '0px', left: '20px', padding: '40px', position: 'relative'}}>
-    <MuiThemeProvider muiTheme={muiTheme}>
-      <PatientDetail 
-        id="newPatientCard"
-        patient={janeDoe}
-        patientId="9ioLMPSkEmzNWtvra"
-        fhirVersion="R4"
-        onDelete={function(){ console.log("On Delete!")}}
-        onUpsert={function(){ console.log("On Upsert!")}}
-        onSave={function(){ console.log("On Save!")}}
-        onCancel={function(){ console.log("On Cancel!")}}
-        buttons={
+function App() {
+  const [capabilityStatement, setCapabilityStatement] = useState("");
+  const client = new Client({
+    baseUrl: 'https://r3.smarthealthit.org'
+  });
+
+  return (
+    <div name="App">
+      <div style={{width: '100%', height: '100%', top: '0px', left: '20px', padding: '40px', position: 'relative'}}>
+        <MuiThemeProvider muiTheme={muiTheme}>
           <div>
-            <RaisedButton id='updatePatientButton' className='updatePatientButton' label="Save" primary={true} style={{marginRight: '20px'}} />
-            <RaisedButton id='deletePatientButton' label="Delete" />
-          </div>
-        }
-      />
+            <header name="App-header">
+              <RaisedButton label="Capability Statement" onClick={onButtonClick.bind(this, client, setCapabilityStatement)} /><br />
+            </header>
+            
+            <pre style={{height: '200px', backgroundColor: "#eeeeee", border: '1px dashed gray', padding: '10px', marginTop: '10px', marginBottom: '10px'}}>
+              { capabilityStatement }
+            </pre>
 
-      {/* <PatientTable
-        id="activePatientsTable"
-        patients={patients}
-        fhirVersion="R4"
-        showActionButton={false}
-        hideToggle={true}
-        hideActionIcons={false}
-        hideIdentifier={false}
-        hideActive={false}
-        hideMaritalStatus={false}
-        hideLanguage={false}
-        hideSpecies={true}
-        paginationLimit={100}
-        onRowClick={function(){console.log("Row Clicked!")}}
-        onCellClick={function(){console.log("Cell Clicked!")}}
-        onActionButtonClick={function(){console.log("Action Button Clicked!")}}
-        onMetaClick={function(scope, patient){          
-          alert('Patient ID: ' + get(patient, '_id'));      
-        }}
-        actionButtonLabel="Action!"
-       /> */}
-    </MuiThemeProvider>  
-  </div>
-);
+            <div style={{border: '1px solid lightgray', marginTop: '10px', marginBottom: '10px'}}>
+              <ObservationTable
+                observations={ observations } 
+                hideValue={false}
+                hideBarcodes={false}
+                hideComparator={false}
+                hideSubjects={false}
+                hidePatientName={false}
+                multiline={false}
+                hideDevices={true}
+                onRemoveRecord={function(_id){
+                  alert('Removing ' + _id)
+                }}
+                hideCheckboxes={true}
+              />
+            </div>
+            {/* <PatientDetail 
+              id="newPatientCard"
+              patient={janeDoe}
+              patientId="9ioLMPSkEmzNWtvra"
+              fhirVersion="R4"
+              onDelete={function(){ console.log("On Delete!")}}
+              onUpsert={function(){ console.log("On Upsert!")}}
+              onSave={function(){ console.log("On Save!")}}
+              onCancel={function(){ console.log("On Cancel!")}}
+            /> */}
+          </div>
+        </MuiThemeProvider>  
+      </div>
+
+
+    </div>
+  );
+}
 
 export default App;
